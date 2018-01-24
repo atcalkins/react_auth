e3592388f3f09775b7c794277bd71592505f755d
Tyler Calkins <atcalkins@outlook.com>
2018-01-24 17:50:39 -0500

    install firebase with web setup and set up text input with state

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common';
+import firebase from 'firebase';
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAWJ2PnaqwrcpvNqhaCOYc0ODxgOw_97mA",
+      authDomain: "auth-94f65.firebaseapp.com",
+      databaseURL: "https://auth-94f65.firebaseio.com",
+      projectId: "auth-94f65",
+      storageBucket: "auth-94f65.appspot.com",
+      messagingSenderId: "236809530824"
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>An App!</Text>
+        <Header headerText="Authentication" />
+        <LoginForm />
       </View>
     );
   }
