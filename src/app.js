93369419154dab2247ecd95a34df6e613baf6e04
Tyler Calkins <atcalkins@outlook.com>
2018-01-30 17:14:54 -0500

    add input, verification, error handling, autocorret handling, and activity spinner

--- src/app.js
+++ src/app.js
@@ -1,29 +1,61 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Header } from './components/common';
+import { View } from 'react-native';
 import firebase from 'firebase';
-import LoginForm from './components/LoginForm'
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
-  componentWillMount() {
+  state = { loggedIn: null };
+
+  componentWillMount (){
     firebase.initializeApp({
-      apiKey: "AIzaSyAWJ2PnaqwrcpvNqhaCOYc0ODxgOw_97mA",
-      authDomain: "auth-94f65.firebaseapp.com",
-      databaseURL: "https://auth-94f65.firebaseio.com",
-      projectId: "auth-94f65",
-      storageBucket: "auth-94f65.appspot.com",
-      messagingSenderId: "236809530824"
+        apiKey: 'AIzaSyCnz8V-MuEiOWcs3oyBBxyO6BCjpAGJm7o',
+        authDomain: 'react-native-auth-284c7.firebaseapp.com',
+        databaseURL: 'https://react-native-auth-284c7.firebaseio.com',
+        storageBucket: 'react-native-auth-284c7.appspot.com',
+        messagingSenderId: '1049444233116'
     });
+
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return <View style={styles.buttonContainerStyle}>
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        </View>;
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
 }
+
+const styles = {
+  buttonContainerStyle: {
+    flexDirection: 'row',
+    height: 40
+  }
+};
 
 export default App;
